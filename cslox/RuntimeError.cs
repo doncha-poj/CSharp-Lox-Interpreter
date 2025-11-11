6a4a321d570d7a57b84b132ebc457106987f3888
Donald Chanthirath <chanthirathdonald@gmail.com>
2025-11-10 20:58:56 -0600

    created runtimeerror.cs

---
+++ cslox/RuntimeError.cs
@@ -0,0 +1,18 @@
+﻿namespace cslox
+{
+    /// <summary>
+    /// Represents an error that occurs during the runtime execution of a program.
+    /// </summary>
+    /// <remarks>This exception is typically used to signal errors that occur during the interpretation or
+    /// execution of code, such as invalid operations or unexpected runtime conditions. The <see cref="Token"/> property
+    /// provides additional context about the location in the source code where the error occurred.</remarks>
+    public class RuntimeError : System.Exception
+    {
+        public readonly Token Token;
+
+        public RuntimeError(Token token, string message) : base(message)
+        {
+            Token = token;
+        }
+    }
+}
