f985dffa48cfc917a4d7c9111dc3c3cea576092c
Donald Chanthirath <chanthirathdonald@gmail.com>
2025-11-10 19:31:39 -0600

    implemented AstPrinter.cs

--- cslox/AstPrinter.cs
+++ cslox/AstPrinter.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace cslox
 {
-    internal class AstPrinter
+    // This class implements the visitor interface.
+    // It returns a string.
+    public class AstPrinter : IExprVisitor<string>
     {
+        // Public entry point
+        public string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        // --- Visitor Implementations ---
+
+        public string VisitBinaryExpr(Binary expr)
+        {
+            // Recursively calls Parenthesize for left, op, and right
+            return Parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right);
+        }
+
+        public string VisitGroupingExpr(Grouping expr)
+        {
+            return Parenthesize("group", expr.Expression);
+        }
+
+        public string VisitLiteralExpr(Literal expr)
+        {
+            if (expr.Value == null) return "nil";
+            return expr.Value.ToString();
+        }
+
+        public string VisitUnaryExpr(Unary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, expr.Right);
+        }
+
+        /// <summary>
+        /// Wraps an expression and its sub-expressions in parentheses.
+        /// </summary>
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("(").Append(name);
+            foreach (var expr in exprs)
+            {
+                builder.Append(" ");
+                // This is the recursion: we call Accept on the sub-expression
+                // which will, in turn, call one of our Visit methods.
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
     }
 }
