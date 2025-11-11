f41ec8463592965bd75098d0ebf8dc4e9706824a
Donald Chanthirath <chanthirathdonald@gmail.com>
2025-11-10 21:34:26 -0600

    Created Environment.cs and Stmt.cs

---
+++ cslox/Environment.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace cslox
+{
+    public class Environment
+    {
+        private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Defines a new variable in the current scope.
+        /// </summary>
+        public void Define(string name, object value)
+        {
+            // We use [] for assignment, which allows
+            // 'var a = 1; var a = 2;'
+            // This is simple, but we'll improve it later.
+            _values[name] = value;
+        }
+
+        /// <summary>
+        /// Gets the value of a variable.
+        /// Throws a RuntimeError if the variable is not defined.
+        /// </summary>
+        public object Get(Token name)
+        {
+            if (_values.ContainsKey(name.Lexeme))
+            {
+                return _values[name.Lexeme];
+            }
+
+            throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
+        }
+
+        /// <summary>
+        /// Assigns a new value to an *existing* variable.
+        /// Throws a RuntimeError if the variable is not defined.
+        /// </summary>
+        public void Assign(Token name, object value)
+        {
+            if (_values.ContainsKey(name.Lexeme))
+            {
+                _values[name.Lexeme] = value;
+                return;
+            }
+
+            throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
+        }
+    }
+}
+{
+    }
+}
---
+++ cslox/Stmt.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace cslox
+{
+    // 1. The Visitor Interface (for Statements)
+    public interface IStmtVisitor<T>
+    {
+        T VisitExpressionStmt(ExpressionStmt stmt);
+        T VisitPrintStmt(PrintStmt stmt);
+        T VisitVarStmt(VarStmt stmt);
+    }
+
+    // 2. The Abstract Base Class
+    public abstract class Stmt
+    {
+        public abstract T Accept<T>(IStmtVisitor<T> visitor);
+    }
+
+    // 3. The Statement Subclasses
+
+    /// <summary>
+    /// A statement that is just an expression (e.g., "1 + 2;")
+    /// </summary>
+    public class ExpressionStmt : Stmt
+    {
+        public readonly Expr Expression;
+
+        public ExpressionStmt(Expr expression)
+        {
+            Expression = expression;
+        }
+
+        public override T Accept<T>(IExprVisitor<T> visitor)
+        {
+            return visitor.VisitExpressionStmt(this);
+        }
+    }
+
+    /// <summary>
+    /// A print statement (e.g., "print 1 + 2;")
+    /// </summary>
+    public class PrintStmt : Stmt
+    {
+        public readonly Expr Expression;
+
+        public PrintStmt(Expr expression)
+        {
+            Expression = expression;
+        }
+
+        public override T Accept<T>(IExprVisitor<T> visitor)
+        {
+            return visitor.VisitPrintStmt(this);
+        }
+    }
+
+    /// <summary>
+    /// A variable declaration (e.g., "var a = 1;")
+    /// </summary>
+    public class VarStmt : Stmt
+    {
+        public readonly Token Name;
+        public readonly Expr Initializer; // Can be null
+
+        public VarStmt(Token name, Expr initializer)
+        {
+            Name = name;
+            Initializer = initializer;
+        }
+
+        public override T Accept<T>(IStmtVisitor<T> visitor)
+        {
+            return visitor.VisitVarStmt(this);
+        }
+    }
+}
